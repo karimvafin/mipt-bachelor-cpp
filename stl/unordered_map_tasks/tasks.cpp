2ec3e178022dcce16b8a984471ae977e6eca253c
shamsutdinovrad <shamsutdinovrad@mail.ru>
2026-04-10 02:35:25 +0300

    Решил задачки на map

--- stl/unordered_map_tasks/tasks.cpp
+++ stl/unordered_map_tasks/tasks.cpp
@@ -1,6 +1,7 @@
 #include "tasks.h"
 
 #include <algorithm>
+#include <iostream>
 
 // =============================================================================
 // Задания по std::unordered_map
@@ -15,7 +16,11 @@
 // Пример: count_elements({}) == {}
 // -----------------------------------------------------------------------------
 std::unordered_map<int, int> count_elements(const std::vector<int>& v) {
-    throw std::runtime_error("Not implemented");
+    std::unordered_map<int, int> count_map;
+    for (auto i : v) {
+        count_map[i]++;
+    }
+    return count_map;
 }
 
 // -----------------------------------------------------------------------------
@@ -27,7 +32,20 @@ std::unordered_map<int, int> count_elements(const std::vector<int>& v) {
 // Пример: most_frequent({4, 4, 5, 5}) == 4
 // -----------------------------------------------------------------------------
 int most_frequent(const std::vector<int>& v) {
-    throw std::runtime_error("Not implemented");
+    if (v.size() == 0) throw std::invalid_argument("Вектор пуст");
+    std::unordered_map<int, int> count_map = count_elements(v);
+
+    int popular_key = -1, cnt = -1;
+    for (const auto& [key, value] : count_map ) {
+        if (value > cnt) {
+            popular_key = key;
+            cnt = value;
+        }
+        else if (value == cnt && popular_key > key) {
+            popular_key = key;
+        }
+    }
+    return popular_key;
 }
 
 // -----------------------------------------------------------------------------
@@ -40,7 +58,18 @@ int most_frequent(const std::vector<int>& v) {
 // Подсказка: используйте unordered_map для решения за O(n).
 // -----------------------------------------------------------------------------
 std::vector<int> two_sum(const std::vector<int>& v, int target) {
-    throw std::runtime_error("Not implemented");
+    std::unordered_map<int, int> indexes;
+    for (int i = 0; i < v.size(); i++) {
+        indexes[v[i]] = i + 1;
+    }
+    for (auto n : v) {
+        if (indexes[target - n] != 0 && indexes[target - n] != indexes[n]){
+            int a = indexes[n] - 1, b = indexes[target - n] - 1;
+            std::vector<int> answer = { std::min(a, b) , std::max(a, b) };
+            return answer;
+        }
+    }
+    return {};
 }
 
 // -----------------------------------------------------------------------------
@@ -53,7 +82,20 @@ std::vector<int> two_sum(const std::vector<int>& v, int target) {
 // Подсказка: у анаграмм одинаковый набор символов — используйте его как ключ.
 // -----------------------------------------------------------------------------
 std::vector<std::vector<std::string>> group_anagrams(const std::vector<std::string>& words) {
-    throw std::runtime_error("Not implemented");
+    std::unordered_map<std::string, std::vector<std::string>> group_map;
+
+    for (auto word : words) {
+        std::string sorted_word = word;
+        std::sort(sorted_word.begin(), sorted_word.end());
+        group_map[sorted_word].push_back(word);
+    }
+
+    std::vector<std::vector<std::string>> answer;
+    for (const auto& [key, value] : group_map) {
+        answer.push_back(value);
+    }
+
+    return answer;
 }
 
 // -----------------------------------------------------------------------------
@@ -66,5 +108,33 @@ std::vector<std::vector<std::string>> group_anagrams(const std::vector<std::stri
 // Пример: top_k_frequent({1}, 1) == {1}
 // -----------------------------------------------------------------------------
 std::vector<int> top_k_frequent(const std::vector<int>& v, int k) {
-    throw std::runtime_error("Not implemented");
+    std::unordered_map<int, int> count_map;
+    std::unordered_map<int, std::vector<int>> index_map;
+    int cnt = 0, k_cnt = 0, now_cnt = v.size();
+
+    for (auto i : v) {
+        count_map[i]++;
+        if (count_map[i] == 1) cnt++;
+    }
+
+    if (k <= 0 || k > cnt) throw std::invalid_argument("Неподходящий k");
+
+    for (const auto& [key, value] : count_map) {
+        index_map[value].push_back(key);
+    }
+    
+    std::vector<int> answer;
+    while (k_cnt != k) {
+        if (index_map.find(now_cnt) != index_map.end()) {
+
+            int max_range = std::min(int(index_map[now_cnt].size()), k - k_cnt);
+            for (int i = 0; i < max_range; i++) {
+                answer.push_back(index_map[now_cnt][i]);
+                k_cnt++;
+            }
+        }
+        now_cnt--;
+    }
+
+    return answer;
 }
