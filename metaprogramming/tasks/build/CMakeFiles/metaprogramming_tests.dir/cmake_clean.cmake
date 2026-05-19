9adfaf5b554c64a6ea98151ba276eac8ccf96c36
Анастасия  Гондаренко <anastasiagondarenko@MacBook-Air-7.local>
2026-05-19 22:22:26 +0300

    last homework

---
+++ metaprogramming/tasks/build/CMakeFiles/metaprogramming_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/metaprogramming_tests.dir/tests.cpp.o"
+  "CMakeFiles/metaprogramming_tests.dir/tests.cpp.o.d"
+  "metaprogramming_tests"
+  "metaprogramming_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/metaprogramming_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ metaprogramming/tasks/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anastasiagondarenko/Downloads/mipt-bachelor-cpp/metaprogramming/tasks
+# Build directory: /Users/anastasiagondarenko/Downloads/mipt-bachelor-cpp/metaprogramming/tasks/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(metaprogramming_tests "/Users/anastasiagondarenko/Downloads/mipt-bachelor-cpp/metaprogramming/tasks/build/metaprogramming_tests")
+set_tests_properties(metaprogramming_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/anastasiagondarenko/Downloads/mipt-bachelor-cpp/metaprogramming/tasks/CMakeLists.txt;13;add_test;/Users/anastasiagondarenko/Downloads/mipt-bachelor-cpp/metaprogramming/tasks/CMakeLists.txt;0;")
